6422425f943ddc3d3313aa23ee2143658e582927
Eiler Gomez <gomezeiler250@gmail.com>
2026-02-16 16:16:56 -0600

    segundo commit a la main

---
+++ Practica1AyD2/src/main/java/com/e/gomez/Practica1AyD2/Practica1AyD2Application.java
@@ -0,0 +1,13 @@
+package com.e.gomez.Practica1AyD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1AyD2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1AyD2Application.class, args);
+	}
+
+}
